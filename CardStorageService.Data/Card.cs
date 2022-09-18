0a550640431ea25deb9195865ca82cf93544c201
k414bin <k414bin@yandex.ru>
2022-09-18 12:58:04 +0300

    All Nuget packages added

--- CardStorageService.Data/Card.cs
+++ CardStorageService.Data/Card.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace CardStorageService.Data
 {
-    internal class Card
+    public class Card
     {
     }
 }
--- CardStorageService.Data/Client.cs
+++ CardStorageService.Data/Client.cs
@@ -3,10 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace CardStorageService.Data
 {
-    internal class Client
+    [Table("clients")]
+    public class Client
     {
+        [Key, DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int ClientId { get; set; }
     }
 }
