68fa8eeb0679970fb11f525fec674e20e103d504
k414bin <k414bin@yandex.ru>
2022-10-10 22:32:34 +0300

    Full working lesson code ADD

---
+++ FullSearchSamples/Sample01.cs
@@ -0,0 +1,42 @@
+﻿using FullSearchSamples.Services.Impl;
+using FullSearchSamples.Services;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace FullSearchSamples
+{
+    internal class Sample01
+    {
+        static void Main(string[] args)
+        {
+            var host = Host.CreateDefaultBuilder(args)
+                .ConfigureServices(services => {
+
+                    #region Configure EF DBContext Service (CardStorageService Database)
+
+                    services.AddDbContext<DocumentDbContext>(options =>
+                    {
+                       options.UseSqlServer(@"data source=DESKTOP-QDKASVN\\SQLEXPRESS;initial catalog=CardStorageService;User Id=CardStorageServiceAdmin;Password=admin12345;MultipleActiveResultSets=True;App=EntityFramework");
+
+                    });
+
+                    #endregion
+
+                    #region Configure Repositories
+
+                    services.AddTransient<IDocumentRepository, DocumentRepository>();
+
+                    #endregion
+
+
+                })
+                .Build();
+
+            // Сохраним документы в БД
+            host.Services.GetRequiredService<IDocumentRepository>().LoadDocuments();
+
+
+        }
+    }
+}
